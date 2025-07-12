7831535d97a3df7be079c0bda2bba2651834c69b
Joylinton04 <adujoy05@gmail.com>
2025-07-12 23:59:57 +0000

    added connections

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 import express from 'express'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js';
 import dotenv from "dotenv";
 import cors from 'cors'
+import productRoute from './route/product.route.js';
 import { jwtCheck } from './middleware/auth.middleware.js';
 
 dotenv.config();
 
-
 const app = express()
 
+connectDB()
+connectCloudinary()
+
+
 app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use('/api/product', productRoute)
 
-
-app.get('/protected', jwtCheck,(req, res) => {
-  res.send('This is a protected route')
+app.get('/', (req,res) => {
+  res.send("API working...")
 })
+app.get('/protected', jwtCheck,(req,res) => {
+  res.send("Authorized...")
+})
+
+
 
 app.use((req, res, next)=> {
   const error = new Error('Not found')
