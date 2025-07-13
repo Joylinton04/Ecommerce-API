297d08ec0f137e19811e08788912221d636ced3b
Joylinton04 <adujoy05@gmail.com>
2025-07-13 20:40:27 +0000

    delete product route

--- route/product.route.js
+++ route/product.route.js
@@ -4,21 +4,31 @@ import {
   deleteProduct,
   listProduct,
   singleProduct,
+  updateProduct,
 } from "../controller/product.controller.js";
 import upload from "../middleware/multer.js";
+import { jwtCheck } from "../middleware/auth.middleware.js";
 
 const productRoute = express.Router();
 
-productRoute.post("/", listProduct);
+productRoute.get("/", listProduct);
 productRoute.post(
   "/add",
-    upload.fields([
-      { name: "image1", maxCount: 1 },
-      { name: "image2", maxCount: 1 },
-    ]),
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+  ]),
   addProduct
 );
-productRoute.post("/delete",deleteProduct);
-productRoute.post("/:id", singleProduct);
+productRoute.delete("/:id", deleteProduct);
+productRoute.get("/:id", singleProduct);
+productRoute.put(
+  "/:id",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+  ]),
+  updateProduct
+);
 
 export default productRoute;
