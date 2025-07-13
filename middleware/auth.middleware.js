c0b06f6c7ec5c8a8c535b6d1d6b742931b1c2cf5
Joylinton04 <adujoy05@gmail.com>
2025-07-13 20:40:47 +0000

    get user middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import { expressjwt } from "express-jwt";
 import jwksRsa from "jwks-rsa";
+import axios from "axios";
+
 
 dotenv.config()
 
@@ -15,4 +17,47 @@ export const jwtCheck = expressjwt({
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
+
+export const verifyUser = async (req, res, next) => {
+  const accessToken = req.headers.authorization.split(" ")[1];
+  if (!accessToken)
+    return res.status(404).json({ success: false, message: "No token found" });
+  try {
+    const response = await axios.get(
+      `https://${process.env.AUTH0_DOMAIN}/userinfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    req.user = response.data;
+    next()
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const verifyAdmin = async (req, res, next) => {
+  const accessToken = req.headers.authorization.split(" ")[1];
+  if (!accessToken)
+    return res.status(404).json({ success: false, message: "No token found" });
+  try {
+    const response = await axios.get(
+      `https://${process.env.AUTH0_DOMAIN}/claims/user`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    // req.user = response.data;
+    // console.log(response.data)
+    next()
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
 
