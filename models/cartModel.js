9924b2dec57a7eb808298d8201cc466bae49a16e
Joylinton04 <adujoy05@gmail.com>
2025-07-14 23:54:34 +0000

    cart model type correction

---
+++ models/cartModel.js
@@ -0,0 +1,41 @@
+// models/Cart.js
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const CartItemSchema = new Schema({
+    product: {
+        type: String,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, { _id: false }); // Don't create an _id for subdocuments if not needed
+
+const CartSchema = new Schema({
+    user: {
+        type: String,
+        ref: 'User',
+        required: false,
+        unique: true,
+    },
+    items: [CartItemSchema], // Array of embedded cart items
+    lastModified: {
+        type: Date,
+        default: Date.now
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const cartModel = mongoose.models.Cart || mongoose.model('Cart', CartSchema);
+export default cartModel
