b52b06bc7611422beea8bc14c8bb16d6b6088f8a
Joylinton04 <adujoy05@gmail.com>
2025-07-15 11:10:37 +0000

    cart route handles

--- route/cart.route.js
+++ route/cart.route.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { addCart } from '../controller/cart.controller.js';
+import { addCart, clearCart, getCart, removeCartItem, updateCartItem } from '../controller/cart.controller.js';
 import { jwtCheck, verifyUser } from '../middleware/auth.middleware.js';
 
 const cartRoute = express.Router()
 
+cartRoute.post('/', jwtCheck, verifyUser,getCart)
 cartRoute.post('/add', jwtCheck, verifyUser,addCart)
+cartRoute.post('/update', jwtCheck, verifyUser, updateCartItem)
+cartRoute.post('/clear', jwtCheck, verifyUser, clearCart)
+cartRoute.post('/remove', jwtCheck, verifyUser, removeCartItem)
 
 
 export default cartRoute;
