7bd6bce2fc8da6019dd0f54f88285b3a9a74498e
Joylinton04 <adujoy05@gmail.com>
2025-07-17 21:52:19 +0000

    cartModel refactor

--- models/cartModel.js
+++ models/cartModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const CartItemSchema = new Schema({
-    product: {
+    productId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product',
         required: true
