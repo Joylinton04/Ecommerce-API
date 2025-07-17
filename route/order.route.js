4a9dc396093d7062e46f1d06c017caebac04c5aa
Joylinton04 <adujoy05@gmail.com>
2025-07-17 21:51:48 +0000

    cancel order handler

--- route/order.route.js
+++ route/order.route.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { placeOrderCOD } from "../controller/order.controller.js";
-import {jwtCheck, verifyUser} from '../middleware/auth.middleware.js'
+import {
+  cancelOrder,
+  orderHistory,
+  placeOrderCOD,
+} from "../controller/order.controller.js";
+import { jwtCheck, verifyUser } from "../middleware/auth.middleware.js";
 
-const orderRoute = express.Router()
+const orderRoute = express.Router();
 
 //user features
-orderRoute.post('/place', jwtCheck, verifyUser, placeOrderCOD)
-
-
-
+orderRoute.post("/place", jwtCheck, verifyUser, placeOrderCOD);
+orderRoute.post("/history", jwtCheck, verifyUser, orderHistory);
+orderRoute.post("/cancel", jwtCheck, verifyUser, cancelOrder);
 
 export default orderRoute;
