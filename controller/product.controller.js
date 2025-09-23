ffdc1ab45386fb91aeb0cbf6c36cd93868e4c4d1
Joylinton04 <adujoy05@gmail.com>
2025-09-23 10:28:23 +0000

    increased the number of images to recieve from the client

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -12,13 +12,14 @@ export const addProduct = async (req, res) => {
       sizes,
       bestSeller,
       stock,
-      date,
     } = req.body;
 
     const image1 = req.files.image1 && req.files.image1[0];
     const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-    const images = [image1, image2].filter((item) => item !== undefined);
+    const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
 
     let imageUrls = await Promise.all(
       images.map(async (item) => {
